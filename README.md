f30a5617255a67b043e158c18ca068c903ddd2ed
rene78 <36115705+rene78@users.noreply.github.com>
2024-01-23 16:41:04 +0800

    - Code cleanup - Readme expanded

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# Address balance change notifier (Work-in-progress, not done yet)
-A Node.js Azure Function has been implemented to function as a timer, executing at specified intervals. This function monitors the balance of a Bitcoin address and triggers an email notification if the balance deviates from the expected value.
+# Address Balance Change Notifier
+This script monitors the balance of a Bitcoin address every 24 hours and triggers an email notification if the balance has changed in the last 2 days. This Node.js script has been written to work on Microsofts Functions-as-a-Service (FaaS) offering, specifically [Azure Functions Timer Trigger](https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-timer). It shouldn't be a lot of work to rewrite it to work on a vanilla Node.js backend though. Just implement a setIntervall() method to call the function at specific time intervalls.
 
 # Configuration Variables
-To customize the functionality, you may adjust the following private variables:
+To customize the functionality, you may adjust the following private variables in a local.settings.json file:
 
-- **Address to Monitor:** Specify the Bitcoin address for monitoring.
-- **Baseline Balance:** Set the initial balance, which should match the current balance of the specified address.
-- **Email Credentials:** Provide the email address and password for programmatic email notifications.
+- **"BTCAddressToMonitor":** Bitcoin address to monitor, e.g. `bc1q3vdf3np392k28hze3zjffk4mk2ka8z9r593jxn`
+- **"eMailHost":** Your email host, e.g. `smtp.office365.com`
+- **"eMailUserName":** Your email address, e.g. `john.doe@outlook.com`
+- **"eMailPassword":** Email password, e.g. `password123`
+- **"eMailFrom":** Text that appears in the from field of the email, e.g. `'John Doe' <john.doe@outlook.com>`
+- **"eMailTo":** The email address to which the notification mail is sent, e.g. `max.mustermann@gmail.com`
+
+Here is an example of such a local.settings.json file
+
+```
+{
+  "IsEncrypted": false,
+  "Values": {
+    "BTCAddressToMonitor": "bc1q3vdf3np392k28hze3zjffk4mk2ka8z9r593jxn",
+    "eMailHost": "smtp.office365.com",
+    "eMailUserName": "john.doe@outlook.com",
+    "eMailPassword": "password123",
+    "eMailFrom": "'John Doe' <john.doe@outlook.com>",
+    "eMailTo": "max.mustermann@gmail.com"
+  }
+}
+```
--- src/functions/addressBalanceChangeNotifier.js
+++ src/functions/addressBalanceChangeNotifier.js
@@ -10,7 +10,7 @@ const EMAILFROM = process.env.eMailFrom;
 const EMAILTO = process.env.eMailTo;
 
 app.timer('addressBalanceChangeNotifier', {
-    schedule: '* * 2 * * *',//Run whenever the hour finger is at 02:00 UTC
+    schedule: '0 0 5 * * *',//Run whenever the seconds are at 0, the minutes are at 0 and the hour is 05:00 UTC
     handler: async (myTimer, context) => {
         // context.log('The Timer function has started.');
         //Get address balance
@@ -19,200 +19,7 @@ app.timer('addressBalanceChangeNotifier', {
             const response = await fetch(`https://api.blockcypher.com/v1/btc/main/addrs/${BTCADDRESSTOMONITOR}`);
             const addressData = await response.json();
             // context.log(addressData);
-            // const addressData = {
-            //     "address": "tb1qfz046rg7g2agakg9paxcw3hnmwnhtjdrfkzefy",
-            //     "total_received": 3931507,
-            //     "total_sent": 2557195,
-            //     "balance": 1374312,
-            //     "unconfirmed_balance": 0,
-            //     "final_balance": 1374312,
-            //     "n_tx": 10,
-            //     "unconfirmed_n_tx": 0,
-            //     "final_n_tx": 10,
-            //     "txrefs": [
-            //         {
-            //             "tx_hash": "8dfe6965fb0aa10f9ca9af027a0b345c8c4ea169c5f8c209a1dff2a372aa2aa2",
-            //             "block_height": 2574158,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 44,
-            //             "value": 1000,
-            //             "ref_balance": 1374312,
-            //             "spent": false,
-            //             "confirmations": 262,
-            //             "confirmed": "2024-01-21T00:01:01Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "a5a8ed3df3a83eb250b747f9d7e2975a863ff4b01ee2585596bce63a24ac6a15",
-            //             "block_height": 2573651,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 35,
-            //             "value": 1000,
-            //             "ref_balance": 1373312,
-            //             "spent": false,
-            //             "confirmations": 769,
-            //             "confirmed": "2024-01-17T16:55:03Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "745de752e4698314b52e4f6a2ee55c4c75d6586842665d76320c9816320653d2",
-            //             "block_height": 2573644,
-            //             "tx_input_n": 0,
-            //             "tx_output_n": -1,
-            //             "value": 1051190,
-            //             "ref_balance": 1372312,
-            //             "confirmations": 776,
-            //             "confirmed": "2024-01-17T16:20:25Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "745de752e4698314b52e4f6a2ee55c4c75d6586842665d76320c9816320653d2",
-            //             "block_height": 2573644,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 3,
-            //             "value": 549951,
-            //             "ref_balance": 2423502,
-            //             "spent": false,
-            //             "confirmations": 776,
-            //             "confirmed": "2024-01-17T16:20:25Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "e1fd99126c4ab7d16e969681dbdfcaa1da3fd9ea2faa2ca537dd3ee4a62954a9",
-            //             "block_height": 2572857,
-            //             "tx_input_n": 0,
-            //             "tx_output_n": -1,
-            //             "value": 1420218,
-            //             "ref_balance": 1873551,
-            //             "confirmations": 1563,
-            //             "confirmed": "2024-01-14T21:43:59Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "e1fd99126c4ab7d16e969681dbdfcaa1da3fd9ea2faa2ca537dd3ee4a62954a9",
-            //             "block_height": 2572857,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 3,
-            //             "value": 418979,
-            //             "ref_balance": 3293769,
-            //             "spent": false,
-            //             "confirmations": 1563,
-            //             "confirmed": "2024-01-14T21:43:59Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "671e0dbee53f6a31f4cd2d32c00ac06ebd0f6ab30b642d61778444d8b17f5cac",
-            //             "block_height": 2572807,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 0,
-            //             "value": 1420218,
-            //             "ref_balance": 2874790,
-            //             "spent": true,
-            //             "spent_by": "e1fd99126c4ab7d16e969681dbdfcaa1da3fd9ea2faa2ca537dd3ee4a62954a9",
-            //             "confirmations": 1613,
-            //             "confirmed": "2024-01-14T10:29:17Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "080182d7e5baab6f18fd4d4a4c602b90c64c4fc70490a2914a70882eb60b96d2",
-            //             "block_height": 2572453,
-            //             "tx_input_n": 0,
-            //             "tx_output_n": -1,
-            //             "value": 1102429,
-            //             "ref_balance": 1454572,
-            //             "confirmations": 1967,
-            //             "confirmed": "2024-01-11T21:23:38Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "080182d7e5baab6f18fd4d4a4c602b90c64c4fc70490a2914a70882eb60b96d2",
-            //             "block_height": 2572453,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 3,
-            //             "value": 1051190,
-            //             "ref_balance": 2557001,
-            //             "spent": true,
-            //             "spent_by": "745de752e4698314b52e4f6a2ee55c4c75d6586842665d76320c9816320653d2",
-            //             "confirmations": 1967,
-            //             "confirmed": "2024-01-11T21:23:38Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "c7d75a615541bda596ef2c482910ce11a5a595dee7da77bc88792dabb55e232c",
-            //             "block_height": 2572453,
-            //             "tx_input_n": 0,
-            //             "tx_output_n": -1,
-            //             "value": 1404621,
-            //             "ref_balance": 1505811,
-            //             "confirmations": 1967,
-            //             "confirmed": "2024-01-11T21:23:38Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "c7d75a615541bda596ef2c482910ce11a5a595dee7da77bc88792dabb55e232c",
-            //             "block_height": 2572453,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 3,
-            //             "value": 403382,
-            //             "ref_balance": 2910432,
-            //             "spent": false,
-            //             "confirmations": 1967,
-            //             "confirmed": "2024-01-11T21:23:38Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "42c9343bdda14b6c385008f0b04a0c78e5c054953e45dbca40a679fb8c5be415",
-            //             "block_height": 2572353,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 1,
-            //             "value": 1404621,
-            //             "ref_balance": 2507050,
-            //             "spent": true,
-            //             "spent_by": "c7d75a615541bda596ef2c482910ce11a5a595dee7da77bc88792dabb55e232c",
-            //             "confirmations": 2067,
-            //             "confirmed": "2024-01-11T10:01:02Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "99a11f53c8f3925e8a3ec57a3a059bbba90cdb7da712d98887d6c588450f53a5",
-            //             "block_height": 2572351,
-            //             "tx_input_n": 0,
-            //             "tx_output_n": -1,
-            //             "value": 1104668,
-            //             "ref_balance": 1102429,
-            //             "confirmations": 2069,
-            //             "confirmed": "2024-01-11T09:45:17Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "99a11f53c8f3925e8a3ec57a3a059bbba90cdb7da712d98887d6c588450f53a5",
-            //             "block_height": 2572351,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 3,
-            //             "value": 1102429,
-            //             "ref_balance": 2207097,
-            //             "spent": true,
-            //             "spent_by": "080182d7e5baab6f18fd4d4a4c602b90c64c4fc70490a2914a70882eb60b96d2",
-            //             "confirmations": 2069,
-            //             "confirmed": "2024-01-11T09:45:17Z",
-            //             "double_spend": false
-            //         },
-            //         {
-            //             "tx_hash": "09673bb0df33e9f6f5dc3921ea28767c6e03f49d98f27a9bbcbe1ec39c67480a",
-            //             "block_height": 2572351,
-            //             "tx_input_n": -1,
-            //             "tx_output_n": 0,
-            //             "value": 1104668,
-            //             "ref_balance": 1104668,
-            //             "spent": true,
-            //             "spent_by": "99a11f53c8f3925e8a3ec57a3a059bbba90cdb7da712d98887d6c588450f53a5",
-            //             "confirmations": 2069,
-            //             "confirmed": "2024-01-11T09:45:17Z",
-            //             "double_spend": false
-            //         }
-            //     ],
-            //     "tx_url": "https://api.blockcypher.com/v1/btc/test3/txs/"
-            // }
+
             //Check if a new transaction has happened within the last 2 days. If yes: Send out e-mail.
             const dateOfLatestTransactionAsString = addressData.txrefs[0].confirmed;
             // const dateOfLatestTransactionString = "2024-01-20T13:00:00";
@@ -226,7 +33,6 @@ app.timer('addressBalanceChangeNotifier', {
             const hours = minutes * 60;
             const days = hours * 24;
             const years = days * 365;
-
             const deltaDays = Math.round(deltaMilliseconds / days);//Convert from milliseconds to days
 
             //Balance has changed in the last 2 days. Send out E-mail notification
@@ -245,7 +51,7 @@ app.timer('addressBalanceChangeNotifier', {
 
         // Send E-Mail once balance has changed
         async function sendMail(datesAndBalancesOflast2Transactions) {
-            context.log('Sending email...'); 
+            context.log('Sending email...');
 
             // create reusable transporter object using the default SMTP transport
             let transporter = nodemailer.createTransport({
@@ -276,8 +82,8 @@ app.timer('addressBalanceChangeNotifier', {
                     </tr>
                     <tr style="border-bottom: 1px solid black;">
                         <td style="padding: 5px;">Balance</td>
-                        <td style="border-left: 1px solid black;padding: 5px;">${datesAndBalancesOflast2Transactions[1].balance/1e8} BTC</td>
-                        <td style="border-left: 1px solid black;padding: 5px;">${datesAndBalancesOflast2Transactions[0].balance/1e8} BTC</td>
+                        <td style="border-left: 1px solid black;padding: 5px;">${datesAndBalancesOflast2Transactions[1].balance / 1e8} BTC</td>
+                        <td style="border-left: 1px solid black;padding: 5px;">${datesAndBalancesOflast2Transactions[0].balance / 1e8} BTC</td>
                     </tr>
                     <tr style="border-bottom: 1px solid black;">
                         <td style="padding: 5px;">Transaction Date</td>
